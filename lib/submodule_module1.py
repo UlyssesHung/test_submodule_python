0f60c248987475d83ea21afb69e3f88c439eeaed
HungChenwei <specialhcw@gmail.com>
2018-01-22 10:23:33 +0900

    Update in sub

--- lib/submodule_module1.py
+++ lib/submodule_module1.py
@@ -2,3 +2,5 @@ def test_print():
     print("this is from submodule.")
 def test_print2():
     print("this is from submodule test2.")
+def test_print3():
+    print("this is from submodule test3 from update in sub.")
